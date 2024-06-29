14f0e5e3182ed5049c84ac2edb20a04c6f763627
KatlynDino <dinokatlynannjoy@gmail.com>
2024-06-28 21:51:41 -0700

    Add files via upload

---
+++ Discover.js
@@ -0,0 +1,27 @@
+document.getElementById("readMoreBtn").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Mayon";
+};
+document.getElementById("readMoreBtnPalawan").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Palawan";
+};
+document.getElementById("readMoreBtnBoracay").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Boracay";
+};
+document.getElementById("readMoreBtnChocolateHills").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Chocolate_Hills";
+};
+document.getElementById("readMoreBtnIntramuros").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Intramuros";
+};
+document.getElementById("readMoreBtnBanaue").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Banaue_Rice_Terraces";
+};
+document.getElementById("readMoreBtnBaguio").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Baguio";
+};
+document.getElementById("readMoreBtnVigan").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Vigan";
+};
+document.getElementById("readMoreBtnSamal").onclick = function() {
+  window.location.href = "https://en.wikipedia.org/tagaytay";
+};
